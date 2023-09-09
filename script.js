c2a74755fff8c5e9ffd101800abab3c984679e30
unknown <devGuelec@gmail.com>
2023-09-10 00:00:17 +0200

    there is still bugs

--- script.js
+++ script.js
@@ -34,3 +34,68 @@ function setupSquares(){
         })
     }
 }
+resetButton.addEventListener("click",function(){
+    reset();
+});
+
+function setupMode(){
+    for (let i =0 ; i < modeButtons.length; i++){
+        modeButtons[i].addEventListener("click",function(){
+            for (let i =0 ; i < modeButtons.length; i++){
+            modeButtons[i].classList.remove("selected");
+            }
+            this.classList.add("selected");
+            if(this.textContent == " Easy "){
+                numSquares = 3;
+            }
+            else{
+                numSquares = 6;
+            }
+            reset();
+        });
+        
+    }
+    
+    
+   
+}
+function reset(){
+    colors= genRandomColors(numSquares);
+    pickedColor = chooseColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = "blue";
+    resetButton.textContent = "New Colors";
+    messageDisplay.textContent = "";
+    for(let i = 0 ; i < squares.length; i++){
+        if (colors[i]){
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];
+        }
+        squares[i].style.display = "none";
+    }
+}
+function changeColors(color){
+    for(let i = 0 ; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+        h1.style.backgroundColor = color;
+    }
+}
+
+function chooseColor(){
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function genRandomColors(num){
+    let arr = [];
+    for (let i=0 ; i<num; i++){
+        arr.push(makeColor());
+    }
+    return arr;
+}
+function makeColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")"; 
+}
